8a915e2b90cbe7a996b4158b8efc2b7c598bd051
Louis Tiao <louistiao@gmail.com>
2014-01-14 23:22:39 +1100

    solved problem 1

---
+++ python/problem2.py
@@ -0,0 +1,30 @@
+# 
+# Fast doubling Fibonacci algorithm
+# 
+# Copyright (c) 2013 Nayuki Minase
+# All rights reserved. Contact Nayuki for licensing.
+# http://nayuki.eigenstate.org/page/fast-fibonacci-algorithms
+# 
+
+
+# Returns F(n)
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("Negative arguments not implemented")
+    return _fib(n)[0]
+
+
+# Returns a tuple (F(n), F(n+1))
+def _fib(n):
+    if n == 0:
+        return (0, 1)
+    else:
+        a, b = _fib(n // 2)
+        c = a * (2 * b - a)
+        d = b * b + a * a
+        if n % 2 == 0:
+            return (c, d)
+        else:
+            return (d, c + d)
+            
+print [fibonacci(i) for i in xrange(10)]
