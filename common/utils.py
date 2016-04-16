94a5705f09c69271142b1d2ca5581a988cbc0e3c
Louis Tiao <louistiao@gmail.com>
2016-04-16 16:00:29 +1000

    updated all to python 3, added teaser ends and removed header

--- common/utils.py
+++ common/utils.py
@@ -1,22 +1,24 @@
 from itertools import chain, combinations, count, islice
 from collections import Counter, defaultdict
+from six.moves import map, range, reduce
 
 def prime_factors(n):
-    if n < 1: 
-        raise ValueError('Requires positive nonzero integer') 
+    if n < 1:
+        raise ValueError('Requires positive nonzero integer')
     else:
         prime_factors_ = Counter()
-        
-        if n == 1: 
+
+        if n == 1:
             return prime_factors_
 
-        for p in prime_range(int(n**0.5)+1):
+        for p in prime_range(int(n**0.5) + 1):
             while n % p == 0:
                 prime_factors_[p] += 1
                 n /= p
-        if n > 1: # n has no further prime factors
+        if n > 1:  # n has no further prime factors
             prime_factors_[n] = 1
         return prime_factors_
+
 
 def _prime_sieve_eratosthenes_2():
     factors = defaultdict(set)
@@ -28,18 +30,22 @@ def _prime_sieve_eratosthenes_2():
             factors[n*n].add(n)
             yield n
 
+
 def _prime_sieve_eratosthenes_1(n):
     composites = set()
-    for i in xrange(2, n):
-        if i not in composites: 
+    for i in range(2, n):
+        if i not in composites:
             yield i
-            composites.update(set(xrange(i*i, n, i)))
+            composites.update(set(range(i*i, n, i)))
+
 
 def prime_range(end, start=2):
-	return (p for p in _prime_sieve_eratosthenes_1(end) if p >= start)
+    return (p for p in _prime_sieve_eratosthenes_1(end) if p >= start)
+
 
 reconstruct = lambda c: reduce(lambda x, y: x*y, map(lambda x: x**c[x], c))
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     import doctest
     doctest.testmod()
